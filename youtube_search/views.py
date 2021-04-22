07750e27ec02aa01609fb32cc72e0523268c7487
inWard <inw@ukr.net>
2021-04-22 22:54:35 +0300

    youtube first form

---
+++ youtube_search/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+from .forms import YoutubeForm, DateForm
+from .video import get_videos
+
+
+def youtube(request):
+    form = YoutubeForm()
+    #df = DateForm()
+    content = {}
+    video_data = {}
+    if request.method == 'POST':
+        form = YoutubeForm(request.POST)
+        if form.is_valid():
+            content = form.cleaned_data
+            video_data = get_videos(content)
+    print(video_data)
+
+    return render(request, 'youtube_search/index.html', {'form': form.as_p(), 'content': content,
+                                                         'video_data': video_data,})
